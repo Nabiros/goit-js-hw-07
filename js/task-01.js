26ccdd1165e6f5dbaa1f2d93644e1c80e595969c
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-06-08 16:29:12 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const categoriesRef = document.querySelector('#categories');
+console.log(`В списке ${categoriesRef.children.length} категории.`);
+
+const getListData = list => {
+  [...list.children].forEach(item => {
+    const title = item.querySelector('h2').textContent;
+    const quantity = item.querySelector('ul').children.length;
+    console.log(`Категория: ${title}`);
+    console.log(`Количество элементов: ${quantity}`);
+  });
+};
+getListData(categoriesRef)
